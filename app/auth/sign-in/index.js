7147c9873254a2b8a93a8ddcd46b30df8bb98426
satwikj218 <satwikj21@gmail.com>
2025-03-07 18:47:08 -0500

    Tab Navigation

--- app/auth/sign-in/index.js
+++ app/auth/sign-in/index.js
@@ -30,6 +30,7 @@ export default function SignIn() {
             .then((userCredential) => {
                 // Signed in 
                 const user = userCredential.user;
+                router.replace('/mytrip');
                 console.log(user);
                 // ...
             })
