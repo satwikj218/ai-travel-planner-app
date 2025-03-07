39a01de16b07d10916b16e37cd9c2af3cdf3e4b7
satwikj218 <satwikj21@gmail.com>
2025-03-07 11:15:33 -0500

    Landing Screen UI

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,20 +1,64 @@
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 import React from 'react';
+import { Colors } from '@/constants/Colors';
 
 export default function Login() {
   return (
     <View>
-      <Image source={require('./../assets/images/login.jpg')} 
-        style={{ 
-            width: '100%', 
-            height: '500' 
+      <Image source={require('./../assets/images/login.jpg')}
+        style={{
+          width: '100%',
+          height: 520,
         }} />
-        <View>
-            <Text style = {{
-                fontSize: 28,
-                fontFamily: 'outfit-bold'
-            }}>AI Travel Planner</Text>
+      <View style={styles.container}>
+        <Text style={{
+          fontSize: 30,
+          fontFamily: 'outfit-bold',
+          textAlign: 'center',
+          marginTop: 10,
+        }}>
+          AI Travel Planner
+        </Text>
+
+        <Text style={{
+          fontSize: 17,
+          fontFamily: 'outfit',
+          textAlign: 'center',
+          color: Colors.GRAY,
+          marginTop: 20,
+        }}>
+          Discover your next adventure effortlessly. Personalized itinerraies at your fingertips. Travel smarter with AI-driven insights.
+        </Text>
+
+        <View style={styles.button}>
+          <Text style= {{
+            color: Colors.WHITE,
+            textAlign: 'center',
+            fontFamily: 'outfit',
+            fontSize: 17,
+          }}>
+            Sign In with Google
+          </Text>
         </View>
+
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.WHITE,
+    marginTop: -20,
+    borderTopRightRadius: 30,
+    borderTopLeftRadius: 30,
+    height: '100%',
+    padding: 25,
+  },
+  button:{
+    backgroundColor: Colors.PRIMARY,
+    padding: 15,
+    borderRadius: 99,
+    marginTop: '20%',
+  }
+})
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -2,6 +2,9 @@ const tintColoLight = "#0a7ea4";
 const tintColorDark = '#fff';
 
 export const Colors = {
+    WHITE: '#fff',
+    PRIMARY:'#000',
+    GRAY: '#7d7d7d',
     light: {
         text: '#11181C',
         background: '#fff',
