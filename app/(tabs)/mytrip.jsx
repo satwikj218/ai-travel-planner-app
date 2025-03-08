8fd9eac02560dfa099881eb172007270e73b6d18
satwikj218 <satwikj21@gmail.com>
2025-03-07 19:11:21 -0500

    Home Page (MyTrip)

--- app/(tabs)/mytrip.jsx
+++ app/(tabs)/mytrip.jsx
@@ -1,10 +1,36 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { Colors } from './../../constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
+import StartNewTripCard from '../../components/MyTrips/StartNewTripCard';
 
 export default function MyTrip() {
-  return (
-    <View>
-      <Text>MyTrip</Text>
-    </View>
-  )
+
+    const [userTrips, setUserTrips] = useState([]);
+
+    return (
+        <View style={{
+            padding: 25,
+            paddingTop: 55,
+            backgroundColor: Colors.WHITE,
+            height: '100%',
+        }}>
+            <View style={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignContent: 'center',
+                justifyContent: 'space-between',
+            }}>
+                <Text style={{
+                    fontSize: 35,
+                    fontFamily: 'outfit-bold',
+                }}>
+                    My Trips
+                </Text>
+                <Ionicons name="add-circle" size={50} color="black" />
+            </View>
+
+            {userTrips?.length == 0 ? <StartNewTripCard/> : null}
+        </View>
+    )
 }
